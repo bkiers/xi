4e6654d70d1dd912daa99c2a211dd3eb74d77256
Bart Kiers <bkiers@gmail.com>
2021-02-13 14:32:05 +0100

    Add explanation in startup scripts

--- check-changes.sh
+++ check-changes.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 
+# This script can be started in its own screen at startup.
+# Iin `/etc/rc.local`, for example:
+#   su - bart -c "screen -dmS xi_chk"
+#   su - bart -c "screen -S xi_chk -X stuff 'cd /home/bart/development/xi && ./check-changes.sh\n'"
+
 while [ true ]
 do
   git fetch origin
--- run-prod.sh
+++ run-prod.sh
@@ -1,6 +1,13 @@
 #!/usr/bin/env bash
 
+# This script can be started in its own screen at startup.
+# Iin `/etc/rc.local`, for example:
+#   su - bart -c "screen -dmS xi"
+#   su - bart -c "screen -S xi -X stuff 'cd /home/bart/development/xi && ./run-prod.sh\n'"
+
 export NODE_ENV=production
+
+date > deploy.txt
 
 npm run build
 
